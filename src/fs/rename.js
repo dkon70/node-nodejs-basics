d5b2211e841b106078efc841a2f4744c7b57b947
dkon70 <dkon70@inbox.ru>
2024-01-24 01:43:52 +0700

    feat: implement fs/rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,23 @@
+import * as fs from 'fs';
+import path from 'path';
+import url from 'url';
+
 const rename = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+    const pathToFile = path.join(__dirname, '/files/wrongFilename.txt');
+    const pathToRenamedFile = path.join(__dirname, '/files/properFilename.md');
+    if (fs.existsSync(pathToRenamedFile)) {
+        throw new Error('FS operation failed');
+    }
+    fs.rename(pathToFile, pathToRenamedFile, function(err) {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                throw new Error('FS operation failed');
+            } else {
+                throw new Error(err);
+            }
+        }
+    });
 };
 
 await rename();
