55bb3291f1315fd1b8e66b069fbe98bcbbdf8099
dkon70 <dkon70@inbox.ru>
2024-01-24 02:12:12 +0700

    feat: implement fs:read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import * as fs from 'fs';
+import path from 'path';
+import url from 'url';
+
 const read = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+    const pathToFile = path.join(__dirname, '/files/fileToRead.txt');
+    fs.readFile(pathToFile, { flag: 'r', encoding: 'utf-8' }, function(err, data) {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                throw new Error('FS operation failed');
+            } else {
+                throw new Error(err);
+            }
+        }
+        console.log(data);
+    });
 };
 
 await read();
