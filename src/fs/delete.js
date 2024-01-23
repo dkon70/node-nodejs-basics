f72df6f8f143dcd0cb9213acd6369e84f0f83b8f
dkon70 <dkon70@inbox.ru>
2024-01-24 01:50:33 +0700

    feat: implement fs/delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import * as fs from 'fs';
+import url from 'url';
+import path from 'path';
+
 const remove = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+    const pathToFile = path.join(__dirname, '/files/fileToRemove.txt');
+    fs.rm(pathToFile, function(err) {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                throw new Error('FS operation failed');
+            } else {
+                throw new Error(err);
+            }
+        }
+    });
 };
 
 await remove();
