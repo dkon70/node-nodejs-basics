434798be3dc44aaba24c609cf61a56e4b87989fe
dkon70 <dkon70@inbox.ru>
2024-01-23 23:11:27 +0700

    feat: implement fs/create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import * as fs from 'fs';
+
 const create = async () => {
-    // Write your code here 
+    fs.open('src/fs/files/fresh.txt', 'wx', function(err, file) {
+        if (err) {
+            if (err.code === 'EEXIST') {
+                throw new Error('FS operation failed');
+            } else {
+                throw new Error(err);
+            }
+        }
+
+        fs.writeFile(file, 'I am fresh and young', function(err) {
+            if (err) {
+                throw new Error(err);
+            }
+        });
+    })
 };
 
 await create();
