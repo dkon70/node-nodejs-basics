3dd0315899fe59b5cf922223e112a8d35c967c45
dkon70 <dkon70@inbox.ru>
2024-01-24 00:03:00 +0700

    feat: implement fs/copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,20 @@
+import * as fs from 'fs';
+import path from 'path';
+import url from 'url';
+
 const copy = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+    const pathToFilesFolder = path.join(__dirname, '/files');
+    const pathToFilesCopyFolder = path.join(__dirname, '/files_copy')
+    fs.cp(pathToFilesFolder, pathToFilesCopyFolder, { recursive: true, errorOnExist: true, force: false }, function(err) {
+        if (err) {
+            if (err.name === 'SystemError') {
+                throw new Error('FS operation failed');
+            } else {
+                throw new Error(err);
+            }
+        }
+    });
 };
 
 await copy();
