3aa6c171860d090137ddd3ab86ac771c719c93dd
dkon70 <dkon70@inbox.ru>
2024-01-24 21:06:53 +0700

    feat: implement streams/read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import { createReadStream } from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { stdout } from 'process';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const pathToFile = path.join(__dirname, '/files/fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    const stream = createReadStream(pathToFile, { encoding: 'utf-8' });
+    stream.pipe(stdout);
 };
 
 await read();
