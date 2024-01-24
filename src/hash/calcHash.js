9f42ee342d16da1291ade470bd9980531491a68b
dkon70 <dkon70@inbox.ru>
2024-01-24 21:00:24 +0700

    feat: implement hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+import { createHash } from 'crypto';
+import url from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const pathToFile = path.join(__dirname, '/files/fileToCalculateHashFor.txt');
+
+const hash = createHash('sha256');
+
 const calculateHash = async () => {
-    // Write your code here 
+    fs.readFile(pathToFile, { encoding: 'utf-8' }, function(err, data) {
+        if (err) {
+            throw new Error(err);
+        }
+        hash.update(data);
+        console.log(hash.digest('hex'));
+    });
 };
 
 await calculateHash();
