42233dd6876c0cdf954f71e7ad6db2089284e883
dkon70 <dkon70@inbox.ru>
2024-01-26 01:05:00 +0700

    fix: output fix in cli/args

--- src/cli/args.js
+++ src/cli/args.js
@@ -3,7 +3,7 @@ import process from 'process';
 const parseArgs = () => {
     const cliArgumentsArray = process.argv.slice(2);
     for(let i = 0; i < cliArgumentsArray.length; i+=2) {
-        console.log(`${cliArgumentsArray[i]} is ${cliArgumentsArray[i+1]}`);
+        console.log(`${cliArgumentsArray[i].slice(2)} is ${cliArgumentsArray[i+1]}`);
     }
 };
 
