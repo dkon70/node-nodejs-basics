7d24a27a8e67c0a8eab0b57dc3fffb677ea56045
dkon70 <dkon70@inbox.ru>
2024-01-26 01:04:05 +0700

    fix: fix variable name in cli/env

--- src/cli/env.js
+++ src/cli/env.js
@@ -3,7 +3,7 @@ import process from 'process';
 const parseEnv = () => {
     for(let key in process.env) {
         if (key.startsWith('RSS_')) {
-            console.log(`${key}=${data[key]}`);
+            console.log(`${key}=${process.env[key]}`);
         }
     }
 };
