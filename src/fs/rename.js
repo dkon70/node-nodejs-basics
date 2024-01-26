28cbaaa1ff6532c34f0f51110e7fe9eca4759291
dkon70 <dkon70@inbox.ru>
2024-01-26 21:54:42 +0700

    fix: replaced sync method by async in fs/rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -6,9 +6,11 @@ const rename = async () => {
     const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
     const pathToFile = path.join(__dirname, '/files/wrongFilename.txt');
     const pathToRenamedFile = path.join(__dirname, '/files/properFilename.md');
-    if (fs.existsSync(pathToRenamedFile)) {
-        throw new Error('FS operation failed');
-    }
+    fs.access(pathToRenamedFile, fs.constants.F_OK, function(err) {
+        if(!err) {
+            throw new Error('FS operation failed');
+        }
+    });
     fs.rename(pathToFile, pathToRenamedFile, function(err) {
         if (err) {
             if (err.code === 'ENOENT') {
